643ef6793744a2b52bb6ab801d378e4bf2a90f09
Marcel Galang <marcelgalang@gmail.com>
2016-09-21 13:28:11 +0000

    Remove slugifiable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,5 @@
 class UsersController < ApplicationController
 
-  get'/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    
-    erb :'users/show_user'
-  end
-
   get '/signup' do
     if !logged_in?
       erb :'users/create_user'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ActiveRecord::Base
   has_many :recipients
   has_secure_password
 
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
 end
